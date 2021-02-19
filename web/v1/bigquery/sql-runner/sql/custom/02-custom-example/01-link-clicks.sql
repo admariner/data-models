d40cd7005762e1c4c0dc05d023ee24467b8c3322
colmsnowplow <colm@snowplowanalytics.com>
2021-02-19 19:05:03 +0000

    BigQuery: Replace example customisation (close #46)

---
+++ web/v1/bigquery/sql-runner/sql/custom/02-custom-example/01-link-clicks.sql
@@ -0,0 +1,38 @@
+-- We use entropy templates to allow ourselves easier separation of dev and prod runs.
+CREATE OR REPLACE TABLE {{.scratch_schema}}.link_clicks{{.entropy}} AS(
+  WITH click_count AS(
+    SELECT
+      ev.page_view_id,
+      COUNT(DISTINCT ev.event_id) AS link_clicks,
+
+    FROM {{.scratch_schema}}.events_staged{{.entropy}} ev
+
+    GROUP BY 1
+
+  ), first_and_last AS(
+    SELECT
+      page_view_id,
+
+      FIRST_VALUE(ev.unstruct_event_com_snowplowanalytics_snowplow_link_click_1_0_1.target_url) OVER(PARTITION BY ev.page_view_id
+         ORDER BY ev.derived_tstamp desc
+         ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS first_link_clicked,
+
+      LAST_VALUE(ev.unstruct_event_com_snowplowanalytics_snowplow_link_click_1_0_1.target_url) OVER(PARTITION BY ev.page_view_id
+         ORDER BY ev.derived_tstamp desc
+         ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS last_link_clicked
+
+    FROM {{.scratch_schema}}.events_staged{{.entropy}} ev
+    WHERE event_name = 'page_view'
+  )
+  SELECT
+    a.page_view_id,
+    a.link_clicks,
+    b.first_link_clicked,
+    b.last_link_clicked
+
+  FROM
+    click_count a
+  LEFT JOIN
+    first_and_last b
+  ON a.page_view_id = b.page_view_id
+);
